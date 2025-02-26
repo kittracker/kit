3f619d2439721ac452c10ed3f794e5703780440e
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2025-02-26 15:31:47 +0100

    Issue details page

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,24 +1,67 @@
 window.onload = (event) => {
+}
+
+function issue(id) {
+    const callback = (data) => {
+        document.getElementById("title-bar").innerText = data.title + " #" + data.id;
+        let content = "";
+        content += "<div class=\"standard-dialog center scale-down\">";
+        content += "<h5 class=\"dialog-text\"><i>Description:</i> " + data.description + "</h5>";
+        content += "<h5 class=\"dialog-text\"><i>Status:</i> " + data.status + "</h5>";
+        content += "<h5 class=\"dialog-text\"><i>Created By:</i> " + data.createdBy.username + "</h5>";
+        content += "</div><br/>";
+
+        content += "<div class=\"standard-dialog center scale-down\">";
+        content += "<h1 class=\"modal-text\">Comments</h1>";
+        content += "<div class=\"separator\"></div>";
+        $.each(data.comments, (_, val) => {
+            content += "<h4 class=\"dialog-text\">" + val.author.username + "</h4>"
+            content += "<h5 class=\"dialog-text\">" + val.text + "</h5>"
+            content += "<div class=\"separator\"></div>";
+        });
+        content += "</div><br/>";
+
+        content += "<div class=\"standard-dialog center scale-down\">";
+        content += "<h1 class=\"modal-text\">Links</h1>";
+        content += "<div class=\"separator\"></div>";
+        $.each(data.links, (_, val) => {
+            content += "<h4 class=\"dialog-text\">" + val.title + " #" + val.id  + "</h4>"
+        });
+        content += "</div><br/>";
+
+        document.getElementById("content-list").innerHTML = content;
+    };
+
+    fetch("issues/" + id, "GET", callback);
+}
+
+function projects() {
     document.getElementById("title-bar").innerText = "Projects";
+    const callback = (data) => {
+        console.log(data);
+        let list = "";
+        $.each(data, (_, val) => {
+            list += "<div class=\"standard-dialog center scale-down\">";
+            list += "<h1 class=\"dialog-text\">" + val.name + "</h1>";
+            list += "<div class=\"separator\"></div>";
+            list += "<h5 class=\"dialog-text\"><i>Owner:</i> " + val.owner.username + "</h5>";
+            list += "<h5 class=\"dialog-text\"><i>ID:</i> " + val.id + "</h5>";
+            list += "<h5 class=\"dialog-text\"><i>Description:</i> " + val.description + "</h5>";
+            list += "<h5 class=\"dialog-text\"><i>Archived:</i> " + val.archived + "</h5>";
+            list += "</div><br/>"
+        });
+        document.getElementById("content-list").innerHTML = list;
+    }
+
+    fetch("projects", "GET", callback);
+}
+
+function fetch(url, method, callback) {
     jQuery.ajax({
-        url: "projects",
-        type: "GET",
+        url: url,
+        type: method,
         dataType: "json",
         async: true,
-        success: (data) => {
-            console.log(data);
-            let list = "";
-            $.each(data, (_, val) => {
-                list += "<div class=\"standard-dialog center scale-down\">";
-                list += "<h1 class=\"dialog-text\">" + val.name + "</h1>";
-                list += "<div class=\"separator\"></div>";
-                list += "<h5 class=\"dialog-text\"><i>Owner:</i> " + val.owner.username + "</h5>";
-                list += "<h5 class=\"dialog-text\"><i>ID:</i> " + val.id + "</h5>";
-                list += "<h5 class=\"dialog-text\"><i>Description:</i> " + val.description + "</h5>";
-                list += "<h5 class=\"dialog-text\"><i>Archived:</i> " + val.archived + "</h5>";
-                list += "</div><br/>"
-            });
-            document.getElementById("content-list").innerHTML = list;
-        }
+        success: (data) => { callback(data) }
     });
 }

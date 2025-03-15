34be6fb74268b3ed0dd76509979266c642c80b66
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2025-03-15 12:59:30 +0100

    UI theme

--- src/main/resources/static/views/projectDetails.js
+++ src/main/resources/static/views/projectDetails.js
@@ -1,3 +1,10 @@
+// <li className="list-group-item d-flex justify-content-between align-items-center list-group-item-action"
+//     href="/issues/${issue.id}" data-link aria-current="true">
+//     <h5>${getStatusIcon(issue.status)} <strong>${issue.title}</strong></h5>
+//     <p>${issue.description}</p>
+//     <span className="badge text-bg-primary rounded-pill">${issue.comments.length}</span>
+// </li>
+
 export default async ({id}) => {
     const response = await fetch(`/api/projects/${id}`);
     if (!response.ok) {
@@ -30,9 +37,9 @@ export default async ({id}) => {
     }
 
     return `
-        <div class="container-fluid">
+        <div class="container h-100">
         
-            <div class="row border-bottom m-3">
+            <div class="row border-bottom m-3" style="padding: 3%">
                 <h1>${project.name}</h1>
                 <p class="m-2">${project.description}</p>
             </div>
@@ -42,27 +49,34 @@ export default async ({id}) => {
                 ${project.issues.length > 0 ? `
                     <ul class="list-group scrollarea">
                         ${project.issues.map(issue => `
-                            <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-action" href="/issues/${issue.id}" data-link aria-current="true">
-                                <h5>${getStatusIcon(issue.status)} <strong>${issue.title}:</strong> ${issue.description}</h5>
-                                <span class="badge text-bg-primary rounded-pill">${issue.comments.length}</span>
+                            <li class="list-group-item list-item-action" href="/issues/${issue.id}" data-link>
+                                <br />
+                                <div class="d-flex justify-content-between">
+                                    <h4>${getStatusIcon(issue.status)}</h4>
+                                    <div class="container">
+                                        <h5>${issue.title}</h5>
+                                        <p>${issue.description}</p>
+                                    </div>
+                                    <span class="badge text-bg-primary rounded-pill align-content-center">${issue.comments.length}</span>
+                                </div>
+                                <br />
                             </li>
                         `).join("")}
                     </ul>
-                ` : `<div class="text-center"> <b>No Issues Yet</b> </div>` }
-                </div>
+                ` : `<div class="text-center"> <b>No Issues Yet</b> </div>`}
+                </div> 
             
-            
-                <div class="col-4" style="width: 380px;">
+                <div class="col-4">
                     <a class="d-flex align-items-center flex-shrink-0 p-3 link-body-emphasis text-decoration-none border-bottom">
                         <span class="fs-5 fw-semibold">Collaborators</span>
                     </a>
                     <div class="list-group list-group-flush border-bottom scrollarea">
                         ${project.collaborators.map(user => `
-                            <a class="list-group-item list-group-item-action py-3 lh-sm">
-                                <div class="d-flex w-100 align-items-center justify-content-between">
+                            <a class="list-group-item list-group-item-action py-3 lh-sm list-item-action">
+                                <div class="d-flex w-100 align-items-center justify-content-between text-center">
                                     <strong class="mb-1">${user.username} ${project.owner.username === user.username ? "(owner)" : ""}</strong>
                                 </div>
-                                <div class="col-10 mb-1 small">${user.emailAddress}</div>
+                                <div class="mb-1 small">${user.emailAddress}</div>
                             </a>
                         `).join("")}
                     </div>
--- src/main/resources/static/views/projects.js
+++ src/main/resources/static/views/projects.js
@@ -5,12 +5,11 @@ export default async ()=> {
     return `
         <div class="container">
             ${projects.map(project => `
-                <div class="card mb-3">
+                <div class="card mb-3 text-center project-card" href="/projects/${project.id}" data-link>
                     <div class="card-body">
                         <h5 class="card-title">${project.name}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">${project.owner.username}</h6>
                         <p class="card-text">${project.description}</p>
-                        <a href="/projects/${project.id}" data-link class="btn btn-primary">View Details</a>
                     </div>
                 </div>
             `).join("")}
